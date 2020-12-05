7a9b76a4b82112daedc2dd61674bad75f5ac2e4b
yinuosh <xiaotian027@hotmail.com>
2020-12-05 15:20:41 +0800

    1204

---
+++ algos/BinarayTreeNode.h
@@ -0,0 +1,280 @@
+#pragma once
+
+#include "Header.h"
+
+using namespace std;
+struct BinarayTreeNode {
+	int val;
+	BinarayTreeNode* left;
+	BinarayTreeNode* right;
+	int height;
+
+	BinarayTreeNode(int value, BinarayTreeNode* l, BinarayTreeNode* r) {
+		val = value;
+		left = l;
+		right = r;
+		height = 1;
+	}
+};
+
+
+typedef void (*TreeNodeHandler) (BinarayTreeNode*);
+
+class TreeNodeMethods {
+public:
+
+	static BinarayTreeNode* FromHeapifyArray(int arr[], int len, int i) {
+		// iLeftChild = 2 * i + 1,  iRightChild = 2*i + 1
+		if (i >= len)return NULL;
+		BinarayTreeNode* node = new BinarayTreeNode(arr[i], NULL, NULL);
+		if (2 * i + 1 < len) {
+			node->left = FromHeapifyArray(arr, len, 2 * i + 1);
+			if (2 * 2 + 2 < len) {
+				node->right = FromHeapifyArray(arr, len, 2 * i + 2);
+			}
+		}
+		return node;
+	}
+
+	static void ToHeapifyArray(BinarayTreeNode* root, vector<int>& arr) {
+		queue<BinarayTreeNode*> nodeQ;
+		if (root == NULL)return;
+		nodeQ.push(root);
+		while (!nodeQ.empty()) {
+			BinarayTreeNode* curNode = nodeQ.front();
+			arr.push_back(curNode->val);
+			nodeQ.pop();
+			if (NULL != curNode->left)nodeQ.push(curNode->left);
+			if (NULL != curNode->right)nodeQ.push(curNode->right);
+		}
+	}
+
+	static void BFS(BinarayTreeNode* root, TreeNodeHandler handler) {
+		queue<BinarayTreeNode*> nodeQ;
+		if (root == NULL)return;
+		nodeQ.push(root);
+		while (!nodeQ.empty()) {
+			BinarayTreeNode* curNode = nodeQ.front();
+			handler(curNode);
+			nodeQ.pop();
+			if (NULL != curNode->left)nodeQ.push(curNode->left);
+			if (NULL != curNode->right)nodeQ.push(curNode->right);
+		}
+	}
+
+	static void DFS(BinarayTreeNode* root, TreeNodeHandler handler) {
+		if (root == NULL)return;
+		handler(root);
+		DFS(root->left, handler);
+		DFS(root->right, handler);
+	}
+
+	static BinarayTreeNode* lowestCommonAncestorTopDown(BinarayTreeNode* root, BinarayTreeNode* p, BinarayTreeNode* q) {
+		if (root == NULL)return NULL;
+		if (root == p || root == q)return root;
+
+		BinarayTreeNode* lAncestor = lowestCommonAncestorTopDown(root->left, p, q);
+		BinarayTreeNode* rAncestor = lowestCommonAncestorTopDown(root->right, p, q);
+		if (lAncestor && rAncestor) return root;
+		else if (NULL != lAncestor) return lAncestor;
+		else if (NULL != rAncestor) return rAncestor;
+		else {
+			return NULL;
+		}
+	}
+};
+
+class BinarySearchTree {
+private:
+	BinarayTreeNode* mRoot = NULL;
+	static void printTreeNode(BinarayTreeNode* node) {
+		if (node) cout << node->val << ",";
+	}
+
+	int height(BinarayTreeNode* node) {
+		if (NULL == node)return 0;
+		return node->height;
+	}
+
+	BinarayTreeNode* Exists(BinarayTreeNode* fromNode, int val) {
+		if (NULL == fromNode)return NULL;
+		if (fromNode->val == val)return fromNode;
+		else if (fromNode->val > val) return Exists(fromNode->left, val);
+		else return Exists(fromNode->right, val);
+	}
+	
+	BinarayTreeNode* ll_rotate(BinarayTreeNode* node) {
+		/*
+				A								B					
+			   /                               /  \ 
+			   B                 ==>          C    A
+			  /
+			  C
+		*/
+		if (NULL == node || NULL == node->left)return node;
+		BinarayTreeNode* left = node->left;
+		node->left = left->right;
+		left->right = node;
+
+		node->height = max(height(node->right), height(node->left)) + 1;
+		left->height = max(height(left->left), height(left->right)) + 1;
+
+		return left;
+	}
+
+	BinarayTreeNode* rr_rotate(BinarayTreeNode* node) {
+
+		/*
+				A								   B
+			     \                               /   \
+			      B                 ==>         A     C
+			       \
+			        C
+		*/
+		if (NULL == node || NULL == node->right)return node;
+		BinarayTreeNode* right = node->right;
+		node->right = right->left;
+		right->left = node;
+
+		node->height = max(height(node->left), height(node->right)) + 1;
+		right->height = max(height(right->left), height(right->right)) + 1;
+	}
+
+	BinarayTreeNode* lr_rotate(BinarayTreeNode* node) {
+		/*
+				A								C
+			   /                               /  \
+			   B                 ==>          B    A
+			    \
+			     C
+		*/
+		if (node == NULL)return NULL;
+		node->left = rr_rotate(node->left);
+		return ll_rotate(node);
+	}
+
+	BinarayTreeNode* rl_rotate(BinarayTreeNode* node) {
+		/*
+				A								C
+			      \                               /  \
+				   B                 ==>          B    A
+				  /
+			     C
+		*/
+		if (node == NULL)return NULL;
+		node->right = ll_rotate(node->right);
+		return rr_rotate(node);
+	}
+
+	BinarayTreeNode* Insert(BinarayTreeNode* node, int val) {
+		//if equals, drop
+		if (node == NULL)node = new BinarayTreeNode(val, NULL, NULL);
+		if (node->val == val)return node;;
+		if (node->val > val) {
+			node->left = Insert(node->left, val);
+		}
+		else {
+			node->right = Insert(node->right, val);
+		}
+
+		node->height = max(height(node->left), height(node->right)) + 1;
+		int balance = node->left - node->right;
+		if (balance > 1 && node->left->val > val) { // ll
+			return ll_rotate(node);
+		}
+		if (balance > 1 && node->left->val < val) { // ll
+			return lr_rotate(node);
+		}
+		if (balance < -1 && node->left->val > val) { // ll
+			return rl_rotate(node);
+		}
+		if (balance < -1 && node->left->val < val) { // ll
+			return rr_rotate(node);
+		}
+
+		return node;
+	}
+
+	BinarayTreeNode* BalanceTree(BinarayTreeNode* node) {
+		int balance = height(node->left) - height(node->right);
+		if (balance > 1) {
+			if (height(node->left->left) > height(node->left->right))return ll_rotate(node);
+			else return lr_rotate(node);
+		}
+		if (balance < -1) {
+			if (height(node->right->right) > height(node->right->left))return rr_rotate(node);
+			else return rl_rotate(node);
+		}
+		return node;
+	}
+
+	//return newRoot
+	BinarayTreeNode* Delete(BinarayTreeNode* root, int val, BinarayTreeNode* parent, bool left) {
+		if (root == NULL)return NULL;
+		if (root->val == val) {
+			BinarayTreeNode* newRoot = NULL;
+			if (NULL == root->left && NULL == root->right) {
+				newRoot = NULL;
+			}
+			else if (NULL == root->left) {
+				newRoot = root->right;
+			}
+			else if (NULL == root->right) {
+				newRoot = root->left;
+			}
+			else {
+				BinarayTreeNode* newRootParent = root;
+				newRoot = root->left;
+				while (newRoot->right != NULL) {
+					newRootParent = newRoot;
+					newRoot = newRoot->right;
+				}
+				if (newRoot != root->left)newRootParent->right = NULL;
+			}
+			if (parent != NULL) {
+				if (left)parent->left = newRoot;
+				else parent->right = newRoot;
+				parent->height = max(height(parent->left), height(parent->right)) + 1;
+			}
+			delete root;
+			root = NULL;
+
+			parent = BalanceTree(parent);
+
+			return newRoot;
+		}
+		else if (root->val >= val) return Delete(root->left, val, root, true);
+		else return Delete(root->right, val, root, false);
+	}
+
+public:
+	BinarySearchTree() :mRoot(NULL) {};
+	BinarySearchTree(BinarayTreeNode* root) :mRoot(root) {};
+
+	bool Insert(int val) {
+		if (mRoot == NULL){
+			mRoot = new BinarayTreeNode(val, NULL, NULL);
+			return true;
+		}
+		return Insert(mRoot, val);
+	}
+
+	void Delete(int val) {
+		//if leaf, delete directly
+		//if only left or right child, delete and move up its only child
+		//else move the the max node up to the target
+
+		BinarayTreeNode* replacement = Delete(mRoot, val, NULL, false);
+		if (NULL == mRoot)mRoot = replacement;
+	}
+
+	BinarayTreeNode* Exists(int val) {
+		return Exists(mRoot, val);
+	}
+
+	void PrintAsHeapifyArray() {
+		cout << "[";
+		TreeNodeMethods::BFS(mRoot, BinarySearchTree::printTreeNode);
+		cout << "]";
+	}
+};
--- algos/main.cpp
+++ algos/main.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <ctime>
 #include <vector>
+#include "BinarayTreeNode.h"
 
 using namespace std;
 
@@ -12,53 +13,60 @@ bool IsValidString(const char* str, int len);
 
 std::stack<int> A, B, C;
 std::map<char, int> hanoiMap;
+void printTreeNode(BinarayTreeNode* node);
 
-void PrintStack(std::stack<int> &a) {
-	std::stack<int> copy(a);
-	while (!copy.empty())
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello world!" << std::endl;
+	std::string s = "AABAACAADAABAABA";
+	std::string p = "AABA";
+	auto result = SortFactory::SearchStringKMP(s.c_str(), s.size(), p.c_str(), p.size());
+	for (auto i : result) {
+		std::cout << i << std::endl;
+	}
+
+	string input = "Hi, this is Sky!";
+	char chars[] = "AABAACAADAABAABA";
+	SortFactory::LeftRotateString(chars, s.size(), 4);
+	std::cout << chars << std::endl;
+
+	char chars2[] = "Hi, this is Sky!";
+	SortFactory::ReverseWords(chars2);
+	std::cout << chars2 << std::endl;
+
+	char dStr[] = " d214748364999";
+	int d = SortFactory::StrToInt(dStr);
+	cout << d << std::endl;
+
+	cout << "===========TreeNode=======================" << endl;
+
+	int btArr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+	BinarayTreeNode* root = TreeNodeMethods::FromHeapifyArray(btArr, 9, 0);
+	TreeNodeMethods::BFS(root, printTreeNode);
+	cout << endl;
+	TreeNodeMethods::DFS(root, printTreeNode);
+	cout << endl;
+	vector<int> heapifyVec;
+	TreeNodeMethods::ToHeapifyArray(root, heapifyVec);
+	Util::PrintVector(heapifyVec, "heapifyVec");
+
+	cout << "===========TreeNode=======================" << endl;
+
+	BinarySearchTree tree = BinarySearchTree();
+	tree.PrintAsHeapifyArray();
+	for (int i = 0; i < 10; i++)
 	{
-		std::cout << copy.top() << ",";
-		copy.pop();
+		tree.Insert(i);
+		tree.PrintAsHeapifyArray();
 	}
-	std::cout << std::endl;
+
+	tree.Delete(2);
+	tree.PrintAsHeapifyArray();
+	tree.Delete(7);
+	tree.PrintAsHeapifyArray();
 }
 
-void PrintHanoi() {
-	std::cout << "A:"; PrintStack(A); 
-	std::cout << "B:"; PrintStack(B);
-	std::cout << "C:"; PrintStack(C);
-	std::cout << "=====================" << std::endl;
-}
-
-
-/*
-Search with wildchars *, _, +
-char* source,
-char* pattern;
-
-if(sLen == 0){
-	if(sP == 0)return true;
-	else{
-		p[0] == "*" || "*+"
-	}
-}
-
-loop(i:source)
-1. source[i] == pattern[0] or pattern[0] = '_' 
-	if next not '+': return match(source[i+1], pattern[1], len(pattern -1))
-	if next == '+': 
-		j = i+1；
-		while(s[j] == s[i])
-		{
-			j++;
-			return match(source[j], pattern[1], len(pattern -1))
-		}
-		
-2. pattern[0] == '*' && ( match(source[i+1], pattern[1], len(pattern) -1) || match(source[i], pattern[1], len of pattern - 1)
-3. pattern
-*/
-
-void Hanoi(char &a, char &b, char &c, int moveNum, std::map<char, int> &count) {
+void Hanoi(char& a, char& b, char& c, int moveNum, std::map<char, int>& count) {
 
 	if (moveNum == 1) {
 		/*c.push(a.top());
@@ -80,110 +88,10 @@ void Hanoi(char &a, char &b, char &c, int moveNum, std::map<char, int> &count) {
 	//PrintHanoi();
 }
 
-// next is filled with 0
-void preKMP(const char* pattern, int len, std::vector<int> &next) {
-	next[0] = 0;
-	for (int i = 1; i < len; i++) {
-		int k = next[i - 1];
-		while (k >= 0)
-		{
-			if (pattern[i] == pattern[k])
-			{
-				next[i] = k + 1;
-				break;
-			}
 
-			
-			if (k > 0) {
-				k = next[k - 1];
-			}
-			else {
-				next[i] = 0;
-				break;
-			}
-		}
-	}
-}
-
-void KMP(const char* s, int sLen, const char* p, int pLen) {
-	std::vector<int> next(pLen, 0);
-	preKMP(p, pLen, next);
-	std::cout << "[";
-	for (int k : next)
-	{
-		std::cout << k << ",";
-	}
-	std::cout << "]" << std::endl;
-
-	int j = 0;
-	for (int i = 0; i < sLen; i++) {
-		if (s[i] == p[j]) {
-			j++;
-			if (j == pLen) {
-				std::cout << i - pLen + 1<< std::endl;
-				j = next[pLen - 1];
-			}
-		}
-		else {
-			i--;
-			if (j > 0) j = next[j - 1];
-		}
-
-	}
-}
 
 void printTreeNode(BinarayTreeNode* node) {
 	if (node) cout << node->val << ",";
-}
-
-int main(int argc, char* argv[])
-{
-	std::cout << "Hello world!" << std::endl;
-	std::string s = "AABAACAADAABAABA";
-	std::string p = "AABA";
-	auto result  = SortFactory::SearchStringKMP(s.c_str(), s.size(), p.c_str(), p.size());
-	for (auto i : result) {
-		std::cout << i << std::endl;
-	}
-
-	string input = "Hi, this is Sky!";
-	char chars[] = "AABAACAADAABAABA";
-	SortFactory::LeftRotateString(chars, s.size(), 4);
-	std::cout << chars << std::endl;
-
-	char chars2[] = "Hi, this is Sky!";
-	SortFactory::ReverseWords(chars2);
-	std::cout << chars2 << std::endl;
-
-	char dStr[] = " d214748364999";
-	int d = SortFactory::StrToInt(dStr); 
-	cout << d << std::endl;
-
-	/*char s1[] = "abcde";
-	char p1[] = "cd";
-	bool m = SortFactory::MatchRecursive(s1, p1);
-	cout << m << std::endl;
-
-	while (true) {
-		cout << "s1:";
-		cin >> s1;
-		cout << "p1:";
-		cin >> p1;
-		SortFactory::MatchDP(s1, strlen(s1), p1, strlen(p1));
-		cout << "=========================" << endl;
-	}*/
-
-	cout << "===========TreeNode=======================" << endl;
-
-	int btArr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-	BinarayTreeNode* root = TreeNodeMethods::FromHeapifyArray(btArr, 9, 0);
-	TreeNodeMethods::BFS(root, printTreeNode);
-	cout << endl;
-	TreeNodeMethods::DFS(root, printTreeNode);
-	cout << endl;
-	vector<int> heapifyVec;
-	TreeNodeMethods::ToHeapifyArray(root, heapifyVec);
-	Util::PrintVector(heapifyVec, "heapifyVec");
 }
 
 bool IsValidString(const char* str, int len) {
@@ -219,3 +127,71 @@ bool IsValidString(const char* str, int len) {
 	return s.empty();
 }
 
+// next is filled with 0
+void preKMP(const char* pattern, int len, std::vector<int>& next) {
+	next[0] = 0;
+	for (int i = 1; i < len; i++) {
+		int k = next[i - 1];
+		while (k >= 0)
+		{
+			if (pattern[i] == pattern[k])
+			{
+				next[i] = k + 1;
+				break;
+			}
+
+
+			if (k > 0) {
+				k = next[k - 1];
+			}
+			else {
+				next[i] = 0;
+				break;
+			}
+		}
+	}
+}
+
+void KMP(const char* s, int sLen, const char* p, int pLen) {
+	std::vector<int> next(pLen, 0);
+	preKMP(p, pLen, next);
+	std::cout << "[";
+	for (int k : next)
+	{
+		std::cout << k << ",";
+	}
+	std::cout << "]" << std::endl;
+
+	int j = 0;
+	for (int i = 0; i < sLen; i++) {
+		if (s[i] == p[j]) {
+			j++;
+			if (j == pLen) {
+				std::cout << i - pLen + 1 << std::endl;
+				j = next[pLen - 1];
+			}
+		}
+		else {
+			i--;
+			if (j > 0) j = next[j - 1];
+		}
+
+	}
+}
+
+void PrintStack(std::stack<int>& a) {
+	std::stack<int> copy(a);
+	while (!copy.empty())
+	{
+		std::cout << copy.top() << ",";
+		copy.pop();
+	}
+	std::cout << std::endl;
+}
+
+void PrintHanoi() {
+	std::cout << "A:"; PrintStack(A);
+	std::cout << "B:"; PrintStack(B);
+	std::cout << "C:"; PrintStack(C);
+	std::cout << "=====================" << std::endl;
+}
